b106591dcb7b194d6334444cf6b9a6569fa3399d
SHEF <Shefqetmush@gmail.com>
2023-01-11 13:22:58 -0500

    added more data to be rendered as cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,25 @@
 export default function Card(prop) {
   return (
-    <div className="card--container section">
-      <img src={prop.data.imageUrl} className="card--image" />
-      <div className="card--text-container">
-        <i className="fa-solid fa-location-pin"></i>
-        &nbsp;
-        <span className="spaced-out">{prop.data.location}</span> &nbsp;
-        <a href={prop.data.googleMapsUrl} className="card--gray">
-          View on Google Maps
-        </a>
-        <h1>{prop.data.title}</h1>
-        <div className="card--description-container">
-          <h3>
-            {prop.data.startDate} - {prop.data.endDate}
-          </h3>
-          <p className="card--text-description">{prop.data.description}</p>
+    <>
+      <div className="card--container section">
+        <img src={prop.data.imageUrl} className="card--image" />
+        <div className="card--text-container">
+          <i className="fa-solid fa-location-pin"></i>
+          &nbsp;
+          <span className="spaced-out">{prop.data.location}</span> &nbsp;
+          <a href={prop.data.googleMapsUrl} className="card--gray">
+            View on Google Maps
+          </a>
+          <h1>{prop.data.title}</h1>
+          <div className="card--description-container">
+            <h3>
+              {prop.data.startDate} - {prop.data.endDate}
+            </h3>
+            <p className="card--text-description">{prop.data.description}</p>
+          </div>
         </div>
       </div>
-    </div>
+      {prop.data.key === 3 ? "" : <hr className="card--hr"></hr>}
+    </>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,7 @@
 export default function Nav() {
   return (
     <div className="nav--container">
-      <i class="fa-solid fa-earth-asia nav--globe"></i>
+      <i className="fa-solid fa-earth-asia nav--globe"></i>
       <h2>&nbsp; my travel journal.</h2>
     </div>
   );
