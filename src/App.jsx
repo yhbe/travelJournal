0e42f95a892b47be543ee1bf8567d94ba347ac01
SHEF <Shefqetmush@gmail.com>
2023-01-10 22:49:07 -0500

    Card component complete and card stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
 import Nav from "./components/Nav";
+import Card from "./components/Card";
+import Data from "../data.js";
 
 function App() {
+  let travelData = Data.map((trip) => {
+    return <Card data={trip} />;
+  });
   return (
-    <>
+    <div className="app--container">
       <Nav />
-    </>
+      {travelData}
+    </div>
   );
 }
 
---
+++ src/components/Card.jsx
@@ -0,0 +1,22 @@
+export default function Card(prop) {
+  return (
+    <div className="card--container section">
+      <img src={prop.data.imageUrl} className="card--image" />
+      <div className="card--text-container">
+        <i className="fa-solid fa-location-pin"></i>
+        &nbsp;
+        <span className="spaced-out">{prop.data.location}</span> &nbsp;
+        <a href={prop.data.googleMapsUrl} className="card--gray">
+          View on Google Maps
+        </a>
+        <h1>{prop.data.title}</h1>
+        <div className="card--description-container">
+          <h3>
+            {prop.data.startDate} - {prop.data.endDate}
+          </h3>
+          <p className="card--text-description">{prop.data.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
