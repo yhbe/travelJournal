ff708a62016c630087f7b9bd009e5cf39d9c8037
SHEF <Shefqetmush@gmail.com>
2023-01-10 21:43:34 -0500

    Nav completed

---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav() {
+  return (
+    <div className="nav--container">
+      <i class="fa-solid fa-earth-asia nav--globe"></i>
+      <h2>&nbsp; my travel journal.</h2>
+    </div>
+  );
+}
